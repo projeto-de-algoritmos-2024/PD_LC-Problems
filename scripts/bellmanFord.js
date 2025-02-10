99942819d1ea9042ee8f0b9e685e5fe75ed1bd05
gustavofbs <guguboasorte@gmail.com>
2025-02-09 21:49:36 -0300

    Definição e verificação da origem do primeiro vértice e ciclos negativos

--- scripts/bellmanFord.js
+++ scripts/bellmanFord.js
@@ -13,5 +13,28 @@ function bellmanFord(edges) {
     verticesList.forEach(vertex => {
         distances[vertex] = Infinity;
         predecessor[vertex] = null;
-    })
+    });
+
+    // Define a origem como o primeiro vértice
+    const source = verticesList[0];
+    distances[source] = 0;
+    
+    // Relaxa as arestas |V| - 1 vezes
+    for (let i = 0; i < verticesList.length - 1; i++) {
+        for (const edge of edges) {
+            if (distances[edge.source] + edge.cost < distances[edge.destination]) {
+                distances[edge.destination] = distances[edge.source] + edge.cost;
+                predecessor[edge.destination] = edge.source;
+            }
+        }
+    }
+    
+    // Verifica ciclos negativos
+    for (const edge of edges) {
+        if (distances[edge.source] + edge.cost < distances[edge.destination]) {
+            return {
+                error: "O grafo contém ciclos negativos"
+            };
+        }
+    }
 }
